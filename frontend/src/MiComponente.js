8808c628bc324a57fba55379384aaa0d7894d260
Sandra Baronetto <smbaronetto@teco.com.ar>
2023-11-10 18:03:16 -0300

    agregando axios.post al procesar archivos

--- frontend/src/MiComponente.js
+++ frontend/src/MiComponente.js
@@ -8,7 +8,9 @@ const MiComponente = () => {
   
   const [nokiaFileSelected, setNokiaFileSelected] = useState('');
   const [posfaFileSelected, setPosfaFileSelected] = useState('');
-  
+  const lblNokiaFile = nokiaFileSelected.name;
+  const lblPosfaFile = posfaFileSelected.name;
+    
   const uploadFile = (files) => {
     // Creating the object of FileReader Class
     var read = new FileReader();
@@ -19,19 +21,26 @@ const MiComponente = () => {
     };
 	// Invoking the readAsText() method by passing the uploaded file as a parameter
     read.readAsText(files[0]);
-    (files[0].name.endsWith('.csv')) ? setNokiaFileSelected(files[0].name) : setPosfaFileSelected(files[0].name)
-    console.log(files[0].name);
+    
+    (files[0].name.endsWith('.csv')) ? setNokiaFileSelected(files[0]) : setPosfaFileSelected(files[0])
+    
   };
 
   const btnProcesar = async() => {
-    const formu = new FormData();
-
-    formu.append("files", nokiaFileSelected);
-    formu.append("files", posfaFileSelected);
+    const formuNk = new FormData();
+    const formuPf = new FormData();
     
-    console.log(formu);
+    formuNk.append("files", nokiaFileSelected);
+    formuPf.append("files", posfaFileSelected);
 
-    await axios.post("http://ec2-34-224-8-97.compute-1.amazonaws.com:5000/", formu)
+    await axios.post("http://ec2-34-224-8-97.compute-1.amazonaws.com:5000/prefa", formuNk)
+    .then(response=>{
+      console.log(response.data);
+    }).catch(error=>{
+      console.log(error);
+    });
+    
+    await axios.post("http://ec2-34-224-8-97.compute-1.amazonaws.com:5000/posfa", formuPf)
     .then(response=>{
       console.log(response.data);
     }).catch(error=>{
@@ -47,9 +56,9 @@ const MiComponente = () => {
             <label> Seleccione el archivo CSV de la <strong>NOKIA</strong> a leer:
               <input 
                 type="text" 
-                id="nokiaFile"              
-                value={ nokiaFileSelected }
-                onChange={e => setNokiaFileSelected(e.target.value)}
+                name="nokiaFile"              
+                value={ lblNokiaFile }
+                onChange={e => setNokiaFileSelected(e.target.nokiaFile)}
               />
             </label>
             <ReactFileReader handleFiles = {uploadFile} fileTypes={".csv"}>
@@ -60,9 +69,9 @@ const MiComponente = () => {
             <label> Seleccione el archivo XLS de la <strong>POSFA</strong> a leer:
               <input 
                 type="text" 
-                id="posfaFile"              
-                value={ posfaFileSelected }
-                onChange={e => setPosfaFileSelected(e.target.value)}
+                name="posfaFile"              
+                value={ lblPosfaFile }
+                onChange={e => setPosfaFileSelected(e.target.posfaFile)}
               />
             </label>
             <ReactFileReader handleFiles = {uploadFile} fileTypes={".xlsx"}>
@@ -71,7 +80,7 @@ const MiComponente = () => {
           </div>
           <div id="form-line">
             <button className="btn" onClick={()=>btnProcesar()}> Procesar archivos </button>
-            <input type="file" name="files" onChange={ (e) => setPosfaFileSelected(e.target.files) }></input>
+            
           </div>     
       </div>
     </>
