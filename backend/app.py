5741cfdbdb9bfcd3b45643fcb52b1bc0f0c2655f
lpossanzini <lpossanzini@teco.com.ar>
2023-11-10 15:18:04 -0300

    Agrego el backend

---
+++ backend/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template, Blueprint
+from routes.principal import principal_bp
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'storage'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+app.register_blueprint(principal_bp)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ backend/routes/principal.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, request
+import os 
+principal_bp = Blueprint('principal', __name__)
+
+@principal_bp.route('/hola',methods=['GET'])
+def index():
+    return "desde un blueprint"
+
+@principal_bp.route('/postfa', methods=['POST'])
+def upload_file_postfa():
+    if 'file' not in request.files:
+        return 'No se proporcionó ningún archivo'
+    file = request.files['file']
+    if file.filename == '':
+        return 'Nombre de archivo no válido'
+    
+    upload_folder = os.path.join(os.getcwd(), 'storage')
+    file.save(os.path.join(upload_folder, "POSTFA"+file.filename))
+    return 'Archivo cargado con éxito'
+
+  
+@principal_bp.route('/prefa', methods=['POST'])
+def upload_file_prefa():
+    if 'file' not in request.files:
+        return 'No se proporcionó ningún archivo'
+    file = request.files['file']
+    if file.filename == '':
+        return 'Nombre de archivo no válido'
+    
+    upload_folder = os.path.join(os.getcwd(), 'storage')
+    file.save(os.path.join(upload_folder, "PREFA"+file.filename))
+
+    return 'Archivo cargado con éxito'
+
+@principal_bp.route('/prefa', methods=['GET'])
+def dowload_file():
+    print("hola")
