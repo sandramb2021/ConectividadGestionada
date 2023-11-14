8377b8e4ee5e9d568b1c7d275703440be03666d1
Sandra Baronetto <smbaronetto@teco.com.ar>
2023-11-14 14:58:47 -0300

    agregarndo http al get

--- frontend/src/MiComponente.js
+++ frontend/src/MiComponente.js
@@ -49,7 +49,7 @@ const MiComponente = () => {
       }
     };
 
-    await axios.get("ec2-54-242-104-125.compute-1.amazonaws.com:5000/", options)
+    await axios.get("http://ec2-54-242-104-125.compute-1.amazonaws.com:5000/", options)
     .then(response=>{
       console.log(response.data);
     }).catch(error=>{
