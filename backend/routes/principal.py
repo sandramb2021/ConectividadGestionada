dd972130805cb3dbc179ec0e9823adeec47b0ab3
lpossanzini <lpossanzini@teco.com.ar>
2023-11-14 15:39:02 -0300

    Cambios

--- backend/routes/principal.py
+++ backend/routes/principal.py
@@ -27,8 +27,6 @@ ec2 = "/storage"
 @principal_bp.route('/postfa', methods=['POST'])
 def upload_file_postfa_csv():
     try:
-        print(request.files)
-        print(request.file)
         if 'file' not in request.files:
             return 'No se proporcionó ningún archivo'
         file = request.files['file']
