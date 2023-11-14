55e916036d320accd04a2dfc64a666da5cf3d7a0
lpossanzini <lpossanzini@teco.com.ar>
2023-11-14 14:51:35 -0300

    Se agrega CORS

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template, Blueprint
 from routes.principal import principal_bp
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)
 UPLOAD_FOLDER = 'storage'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
