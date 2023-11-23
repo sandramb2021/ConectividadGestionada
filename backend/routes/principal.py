8a5d9c91915ef1acb6129ee63e72052d086105eb
lpossanzini <lpossanzini@teco.com.ar>
2023-11-23 10:52:03 -0300

    algo

--- backend/routes/principal.py
+++ backend/routes/principal.py
@@ -73,8 +73,11 @@ def download_file():
 ## RUTA QUE CARGA EL ARCHIVO CSV EN EC2 y S3
 @principal_bp.route('/nokia_s3', methods=['POST'])
 def upload_file_nokia_csv_s3():
-    upload_file_nokia_s3(request)
-    return "Se cargo correctamente el archivo NOKIA"
+    try:
+        upload_file_nokia_s3(request)
+        return "Se cargo correctamente el archivo NOKIA"
+    except Exception as e:
+        return "No se cargo nada",e
 
 ## RUTA QUE CARGA EL ARCHIVO FACTURACION EN EC2 y S3
 @principal_bp.route('/facturacion_s3', methods=['POST'])
